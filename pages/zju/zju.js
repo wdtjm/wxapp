a6028d6b46d5c7b065cef94393f80e41aae5ab3a
zbw <1826077331@qq.com>
2023-12-07 16:48:28 +0800

    moveto backto zhuanfa over

--- pages/zju/zju.js
+++ pages/zju/zju.js
@@ -5,9 +5,57 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    latitude:23.098994,
+    longitude:113.322520,
+    markers:[{
+      id:1,
+      latitude:23.098994,
+      longitude:113.322520,
+      width:30,
+      height:30,
+      title:'腾讯大厦',
+    }]
   },
-
+  moveToLocation:function(event){
+    console.log(event);
+    let that = this;
+    wx.getLocation(
+      {
+        type: 'wgs84',
+        success(res){
+          const latitude = res.latitude
+          const longitude = res.longitude
+          that.setData({
+            latitude:latitude,
+            longitude:longitude,
+            markers:[{
+              id:1,
+              latitude:latitude,
+              width:30,
+              height:30,
+              longitude:longitude,
+              title:'当前位置',
+            }]
+          })
+        }
+      }
+    )
+  },
+  moveBack: function(event){
+    console.log(event);
+    this.setData({
+      latitude: 23.098994,
+      longitude: 113.322520,
+      markers: [{
+        id: 1,
+        latitude: 23.098994,
+        longitude: 113.322520,
+        width:30,
+       height:30,
+        title: '腾讯大厦',
+      }]
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -61,6 +109,17 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage() {
-
+    const promise = new Promise(resolve =>{
+      setTimeout(()=>{
+        resolve({
+          title: '转发'
+        })
+      },2000)
+    })
+    return {
+      title: '转发',
+      path: '/pages/zju/zju',
+      promise
+    }
   }
 })
